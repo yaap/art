2217b6a1986cfb432fc1a48904f4f59b2126c7aa
Vladimir Marko <vmarko@google.com>
2022-12-13 16:46:22 +0000

    CC GC: Clean up class loaders in reclaim phase.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing --jit
    Bug: 38383823
    Change-Id: I82f6cdd78592260c7bd82dd83d3f898ed6ebcafe

--- runtime/gc/collector/concurrent_copying.cc
+++ runtime/gc/collector/concurrent_copying.cc
@@ -1692,8 +1692,6 @@ void ConcurrentCopying::CopyingPhase() {
     if (kVerboseMode) {
       LOG(INFO) << "SweepSystemWeaks done";
     }
-    // Free data for class loaders that we unloaded.
-    Runtime::Current()->GetClassLinker()->CleanupClassLoaders();
     // Marking is done. Disable marking.
     DisableMarking();
     CheckEmptyMarkStack();
@@ -2715,6 +2713,11 @@ void ConcurrentCopying::ReclaimPhase() {
     LOG(INFO) << "GC ReclaimPhase";
   }
   Thread* self = Thread::Current();
+
+  // Free data for class loaders that we unloaded. This includes removing
+  // dead methods from JIT's internal maps. This must be done before
+  // reclaiming the memory of the dead methods' declaring classes.
+  Runtime::Current()->GetClassLinker()->CleanupClassLoaders();
 
   {
     // Double-check that the mark stack is empty.
